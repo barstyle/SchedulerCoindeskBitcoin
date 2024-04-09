06f1cf68edc33ec56c62f7d51a00d301cc9d5f28
Barsty <barstyle@mail.ru>
2024-04-09 13:14:03 +0300

    add AtomicDouble

--- src/main/java/com/coindesk/schedulercoindeskbitcoin/controller/CoinDeskController.java
+++ src/main/java/com/coindesk/schedulercoindeskbitcoin/controller/CoinDeskController.java
@@ -4,6 +4,8 @@ import com.coindesk.schedulercoindeskbitcoin.model.ResponseCoinDesk;
 import com.coindesk.schedulercoindeskbitcoin.service.CoinDeskService;
 import io.micrometer.core.annotation.Timed;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,13 +13,17 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 public class CoinDeskController {
 
     private final CoinDeskService coinDeskService;
 
     @GetMapping("coindesk")
     @Timed("get_controller_request_coindesk_response")
+    @SneakyThrows
     ResponseEntity<ResponseCoinDesk> getResponseCoinDesk() {
+        log.info("Выполнен запрос getResponseCoinDesk() ");
+        Thread.sleep(60_000);
         return new ResponseEntity<>(coinDeskService.getResponseCoinDesk(), HttpStatus.OK);
     }
 
--- src/main/java/com/coindesk/schedulercoindeskbitcoin/service/CoinDeskService.java
+++ src/main/java/com/coindesk/schedulercoindeskbitcoin/service/CoinDeskService.java
@@ -2,28 +2,29 @@ package com.coindesk.schedulercoindeskbitcoin.service;
 
 import com.coindesk.schedulercoindeskbitcoin.apiclient.CoinDeskApiClient;
 import com.coindesk.schedulercoindeskbitcoin.model.ResponseCoinDesk;
+import com.google.common.util.concurrent.AtomicDouble;
 import io.micrometer.core.instrument.MeterRegistry;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
-import java.util.concurrent.atomic.AtomicInteger;
 
 @Service
+@Slf4j
 public class CoinDeskService {
 
     private final CoinDeskApiClient coinDeskApiClient;
-    private AtomicInteger rateBitcoinOfUsd;
+    private final AtomicDouble rateBitcoinOfUsd;
 
     public CoinDeskService(CoinDeskApiClient coinDeskApiClient, MeterRegistry meterRegistry) {
         this.coinDeskApiClient = coinDeskApiClient;
-        rateBitcoinOfUsd = new AtomicInteger();
+        rateBitcoinOfUsd = new AtomicDouble();
         meterRegistry.gauge("BTCtoUSD", rateBitcoinOfUsd);
     }
 
     public ResponseCoinDesk getResponseCoinDesk() {
         ResponseCoinDesk responseCoinDesk = coinDeskApiClient.getResponseCoinDesk();
-
         double rateFloat = responseCoinDesk.bpi.uSD.rate_float;
-        rateBitcoinOfUsd.set((int) rateFloat);
+        rateBitcoinOfUsd.set(rateFloat);
         return responseCoinDesk;
     }
 }
